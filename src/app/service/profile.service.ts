434039156e0abfe76c0fcc7f3fd85847a28f96bc
ArnoldHenry <arnoldomwoyo2@gmail.com>
2018-09-23 22:21:26 +0300

    get json data from github

--- src/app/service/profile.service.ts
+++ src/app/service/profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers} from '@angular/http';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +10,14 @@ export class ProfileService {
   private clientid = '2d1ff9a3204b774face2';
   private clientsecret = '2274b56623bc11c5358758cc595fabcd59b13457';
 
-  constructor(private http:Http) { }
+  constructor(private http:Http) { 
+    console.log("Service works");
+    this.username = 'ArnoldHenry';
+
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/"+this.username+"?client_id="+this.clientid+"&client_secret="+this.clientsecret)
+    .pipe(
+    map(res => res.json()));
+  }
 }
